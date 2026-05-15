f64d3da37e375f3a9726b1f08bfee2d4f40114d2
Kuruto Denzeru <kurtdenzel51@gmail.com>
2026-05-15 08:14:24 -0700

    chore: add .output to .gitignore and remove build artifacts from tracking

--- .output/server/_tanstack-start-manifest_v-B_gR2FCU.mjs
+++
@@ -1,4 +0,0 @@
-const tsrStartManifest = () => ({ routes: { __root__: { filePath: "/Users/kurtcalacday/Documents/Projects/Personal/Inkoro/src/routes/__root.tsx", children: ["/"], assets: [{ tag: "link", attrs: { rel: "stylesheet", href: "/assets/index-qpjIDQXj.css", type: "text/css" } }], preloads: ["/assets/index-DptEBu0Y.js"] }, "/": { filePath: "/Users/kurtcalacday/Documents/Projects/Personal/Inkoro/src/routes/index.tsx", children: void 0, assets: [], preloads: ["/assets/index-D6Gq-XOt.js"] } }, clientEntry: "/assets/index-DptEBu0Y.js" });
-export {
-  tsrStartManifest
-};
